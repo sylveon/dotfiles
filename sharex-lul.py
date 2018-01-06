aa22015ecd0f2659a3434b1e4b11192b205e89b3
Charles Milette <charles.milette@gmail.com>
2018-01-05 20:37:25 -0500

    No shadows finally

--- sharex-lul.py
+++ sharex-lul.py
@@ -21,7 +21,7 @@ def main():
     if len(sys.argv) == 1 or sys.argv[1] == 'full':
         os.system(f'maim -m 10 "{file_path}"')
     elif sys.argv[1] == 'region':
-        os.system(f'maim -m 10 -s | convert - \( +clone -background black -shadow 80x3+0+0 \) +swap -background none -layers merge +repage "{file_path}"')
+        os.system(f'maim -m 10 -s "{file_path}"')
     elif sys.argv[1] == 'active':
         os.system(f'maim -m 10 -i $(xdotool getactivewindow) "{file_path}"')
     else:
