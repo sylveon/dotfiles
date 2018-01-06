cdb3de87bdd03db56223ba60b90a5484646fad07
Charles Milette <charles.milette@gmail.com>
2018-01-06 11:39:41 -0500

    hide cmd duration

--- fish/config.fish
+++ fish/config.fish
@@ -6,6 +6,7 @@ else
 end
 
 set -g theme_display_date no
+set -g theme_display_cmd_duration no
 set -g theme_title_display_process yes
 set -g theme_show_exit_status yes
 set -g fish_prompt_pwd_dir_length 5
