91f697403237b1ff9aa6ce575ca2c1eb340ee541
Charles Milette <charles.milette@gmail.com>
2018-01-05 23:20:42 -0500

    Add fish configs

---
+++ fish/config.fish
@@ -0,0 +1,19 @@
+if test $TERMINOLOGY -eq 1 ^ /dev/null
+    set -g theme_nerd_fonts yes
+    set -g theme_color_scheme solarized
+else
+    set -g theme_color_scheme terminal2-light-black
+end
+
+set -g theme_title_display_process yes
+set -g theme_show_exit_status yes
+set -g fish_prompt_pwd_dir_length 5
+set -x HASTE_SERVER "https://haste.charlesmilette.net"
+set -x THEFUCK_OVERRIDDEN_ALIASES "git"
+
+alias su="sudo su"
+alias ls="ls --color -A"
+alias grep="grep --color"
+
+
+eval (hub alias -s)
